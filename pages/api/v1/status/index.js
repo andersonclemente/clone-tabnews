74281739cc4daa8954e756523d7e6ddf847c4b9c
andersonclemente <clemente.anderson11@gmail.com>
2024-01-11 19:27:41 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,11 @@
-export default function status(request, response) {
-  response.status(200).json({ chave: "teste de http são maneiros" });
+import { rows } from "pg/lib/defaults.js";
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  if (result.rows[0].sum == 2) {
+    response.status(200).json({ msg: "teste de http são maneiros" });
+  } else {
+    response.status(500).json({ msg: "banco de dados não conectado" });
+  }
 }
