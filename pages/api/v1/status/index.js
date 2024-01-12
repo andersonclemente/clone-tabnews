2b1c40a7b17a61ea7dd5a6e114e8b3764c64a93e
andersonclemente <clemente.anderson11@gmail.com>
2024-01-12 18:25:12 -0300

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,9 @@
-import { rows } from "pg/lib/defaults.js";
-import database from "../../../../infra/database.js";
+import database from "infra/database";
 
 export default async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   if (result.rows[0].sum == 2) {
-    response.status(200).json({ msg: "teste de http são maneiros" });
+    response.status(200).json({ msg: "testes de http são maneiros" });
   } else {
     response.status(500).json({ msg: "banco de dados não conectado" });
   }
