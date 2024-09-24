8c82490ea74de2b1913d88e565d5e322078d0c1c
Anderson Clemente <anderson.filipe.clemente@gmail.com>
2024-09-23 22:08:33 -0300

    refact fix bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -9,31 +9,37 @@ export default async function migrations(request, response) {
       .json({ msg: "this endpoint does not allow this method" });
   }
 
-  const dbClient = await database.getNewClient();
-  const defaultMigrationsConfig = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  let dbClient;
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationsConfig);
+  try {
+    dbClient = await database.getNewClient();
+
+    const defaultMigrationsConfig = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationsConfig);
+      return response.status(200).json(pendingMigrations);
+    }
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsConfig,
+        dryRun: false,
+      });
+
+      const statusCode = migratedMigrations.length > 0 ? 201 : 200;
+      return response.status(statusCode).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    return response.status(405).end();
+  } finally {
     dbClient.end();
-    return response.status(200).json(pendingMigrations);
   }
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsConfig,
-      dryRun: false,
-    });
-
-    dbClient.end();
-    const statusCode = migratedMigrations.length > 0 ? 201 : 200;
-    return response.status(statusCode).json(migratedMigrations);
-  }
-
-  return response.status(405).end();
 }
