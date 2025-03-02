20fef7a88ee8e1d9a58ec214ffdacb83ad9f2037
Anderson Clemente <anderson.filipe.clemente@gmail.com>
2025-03-02 08:49:18 -0300

    --amend

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -12,7 +12,7 @@ test("GET to /api/v1/migrations should return 200", async () => {
   const numberMigrationsToRun = fs.readdirSync("infra/migrations").length;
 
   const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(201);
+  expect(response.status).toBe(200);
 
   const responseBody = await response.json();
 
